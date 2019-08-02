912474c40553b89e6072eb7f1602aea19a9eda80
Máté Eckl <mate.eckl@celeraone.com>
2019-08-02 15:28:54 +0200

    Pacman rankmirrors

---
+++ pacman-rankmirrors/README.md
@@ -0,0 +1,9 @@
+# Mirror ranking for pacman in docker
+
+This docker image helps runs the official pacman mirror ranking program which
+ranks mirrors according to download speed from them.
+
+You can pass arguments in the run command, try `docker run -it
+ecklm/pacman-rankmirrors -h` to see what your options are.
+
+For info about the backgrounds, see the [ArchWiki](https://wiki.archlinux.org/index.php/Mirrors#List_by_speed)
---
+++ pacman-rankmirrors/rankmirrors.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -e
+
+country=all
+proto=https
+ipv=4
+
+function print_help {
+	echo "Parameters:
+	-c <country>    Default: all
+	-p <protocol>   http or https Default: https
+	-v <IP version> 4 or 6 Default: 4
+	-h              Print help text"
+}
+
+while [ "$#" -gt 0 ]
+do
+	opt=$1
+	case $opt in
+		-c)
+			country=$2
+			shift 2
+		;;
+		-p)
+			proto=$2
+			shift 2
+		;;
+		-v)
+			ipv=$2
+			shift 2
+		;;
+		*)
+			print_help
+			exit
+		;;
+	esac
+done
+
+set -v
+
+curl "https://www.archlinux.org/mirrorlist/?country=${country}&protocol=${proto}&ip_version=${ipv}" |\
+         sed "s/^#//" | grep -v "#" | rankmirrors -n 10 -
